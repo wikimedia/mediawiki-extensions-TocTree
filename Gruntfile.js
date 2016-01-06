395d32a92114c19844a3f0e5f87a7e9131fe1e9b
Antoine Musso <hashar@free.fr>
2016-01-06 16:26:15 +0100

    Add jshint to 'npm test' command
    Bug: T63640
    Change-Id: Ibae36050a1d8054b7280804513a258bc7cd8960f

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,7 @@
 /*jshint node:true */
 module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
 
 	var conf = grunt.file.readJSON( 'extension.json' );
@@ -17,9 +18,15 @@ module.exports = function ( grunt ) {
 				'**/*.json',
 				'!node_modules/**'
 			]
+		},
+		jshint: {
+			all: [
+				'**/*.js',
+				'!node_modules/**'
+			]
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'jshint', 'jsonlint', 'banana' ] );
 	grunt.registerTask( 'default', 'test' );
 };
