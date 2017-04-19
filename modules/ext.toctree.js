2d7e14f7e5446efa35aaaa93fb341ca3d10d8330
Fomafix <fomafix@googlemail.com>
2017-04-19 18:07:00 +0200

    Integrate .hasClass in the .find selector
    Change-Id: I6554e3d2f0bdfe53932396f74a2bbffa7132be12

--- modules/ext.toctree.js
+++ modules/ext.toctree.js
@@ -35,38 +35,36 @@
 		}
 
 		var $mainUl = $toc.find( 'ul:first' );
-		var $mainList = $toc.find( 'li' );
+		var $mainList = $toc.find( 'li.toclevel-1' );
 
 		$mainList.each( function ( i ) {
-			if ( $( this ).hasClass( 'toclevel-1' ) ) {
-				$( this ).css( 'position', 'relative' );
-				var $subList = $( this ).find( 'ul' );
+			$( this ).css( 'position', 'relative' );
+			var $subList = $( this ).find( 'ul' );
 
-				if ( $subList.length > 0 ) {
-					$mainUl.addClass( 'tocUl' );
+			if ( $subList.length > 0 ) {
+				$mainUl.addClass( 'tocUl' );
 
-					var $toggleLink = $( '<span>' ).addClass( 'toggleSymbol' );
+				var $toggleLink = $( '<span>' ).addClass( 'toggleSymbol' );
 
-					if ( mw.user.options.get( 'toc-expand' ) ) {
-						$toggleLink
-							.text( '-' )
-							.attr( 'title', mw.msg( 'hidetoc' ) );
+				if ( mw.user.options.get( 'toc-expand' ) ) {
+					$toggleLink
+						.text( '-' )
+						.attr( 'title', mw.msg( 'hidetoc' ) );
 
-						$subList.show();
-					} else {
-						$toggleLink
-							.text( '+' )
-							.attr( 'title', mw.msg( 'showtoc' ) );
+					$subList.show();
+				} else {
+					$toggleLink
+						.text( '+' )
+						.attr( 'title', mw.msg( 'showtoc' ) );
 
-						$subList.hide();
-					}
-					$toggleLink.click( processClickEvent );
-
-					var $toggleSpan = $( '<span>' ).addClass( 'toggleNode' );
-					$toggleSpan.append( '[', $toggleLink, ']' );
-
-					$( this ).prepend( $toggleSpan );
+					$subList.hide();
 				}
+				$toggleLink.click( processClickEvent );
+
+				var $toggleSpan = $( '<span>' ).addClass( 'toggleNode' );
+				$toggleSpan.append( '[', $toggleLink, ']' );
+
+				$( this ).prepend( $toggleSpan );
 			}
 		} );
 	}
