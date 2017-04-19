10d4c98015e77bd161442bdc07d2eb73235ab639
Fomafix <fomafix@googlemail.com>
2017-04-19 18:02:13 +0200

    Remove some $x.length > 0 checks
    When $x.length === 0 then $x.addClass( 'foo' ) does nothing and makes no
    error.
    
    Change-Id: I2ede566efe15678430c5887ac60c4c3ab54d41d2

--- modules/ext.toctree.js
+++ modules/ext.toctree.js
@@ -30,49 +30,45 @@
 	function init( $content ) {
 		var $toc = $content.find( '.toc' ).addBack( '.toc' );
 
-		if ( $toc.length > 0 ) {
-			if ( mw.user.options.get( 'toc-floated' ) ) {
-				$toc.addClass( 'tocFloat' );
-			}
-
-			var $mainUl = $toc.find( 'ul:first' );
-			var $mainList = $toc.find( 'li' );
-
-			$mainList.each( function ( i ) {
-				if ( $( this ).hasClass( 'toclevel-1' ) ) {
-					$( this ).css( 'position', 'relative' );
-					var $subList = $( this ).find( 'ul' );
-
-					if ( $subList.length > 0 ) {
-						if ( $mainUl.length > 0 ) {
-							$mainUl.addClass( 'tocUl' );
-						}
-
-						var $toggleLink = $( '<span>' ).addClass( 'toggleSymbol' );
-
-						if ( mw.user.options.get( 'toc-expand' ) ) {
-							$toggleLink
-								.text( '-' )
-								.attr( 'title', mw.msg( 'hidetoc' ) );
-
-							$subList.show();
-						} else {
-							$toggleLink
-								.text( '+' )
-								.attr( 'title', mw.msg( 'showtoc' ) );
-
-							$subList.hide();
-						}
-						$toggleLink.click( processClickEvent );
-
-						var $toggleSpan = $( '<span>' ).addClass( 'toggleNode' );
-						$toggleSpan.append( '[', $toggleLink, ']' );
-
-						$( this ).prepend( $toggleSpan );
-					}
-				}
-			} );
+		if ( mw.user.options.get( 'toc-floated' ) ) {
+			$toc.addClass( 'tocFloat' );
 		}
+
+		var $mainUl = $toc.find( 'ul:first' );
+		var $mainList = $toc.find( 'li' );
+
+		$mainList.each( function ( i ) {
+			if ( $( this ).hasClass( 'toclevel-1' ) ) {
+				$( this ).css( 'position', 'relative' );
+				var $subList = $( this ).find( 'ul' );
+
+				if ( $subList.length > 0 ) {
+					$mainUl.addClass( 'tocUl' );
+
+					var $toggleLink = $( '<span>' ).addClass( 'toggleSymbol' );
+
+					if ( mw.user.options.get( 'toc-expand' ) ) {
+						$toggleLink
+							.text( '-' )
+							.attr( 'title', mw.msg( 'hidetoc' ) );
+
+						$subList.show();
+					} else {
+						$toggleLink
+							.text( '+' )
+							.attr( 'title', mw.msg( 'showtoc' ) );
+
+						$subList.hide();
+					}
+					$toggleLink.click( processClickEvent );
+
+					var $toggleSpan = $( '<span>' ).addClass( 'toggleNode' );
+					$toggleSpan.append( '[', $toggleLink, ']' );
+
+					$( this ).prepend( $toggleSpan );
+				}
+			}
+		} );
 	}
 
 	mw.hook( 'wikipage.content' ).add( init );
