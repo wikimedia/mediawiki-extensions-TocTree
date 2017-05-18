4a6d6eef1a65261347745e55b993d6ee8a1f8445
Umherirrender <umherirrender_de.wp@web.de>
2017-05-18 18:38:06 +0200

    Add phpcs and make pass
    Change-Id: I993df5016c122cc82f7f968b18418171a19c9c8b

--- TocTree.i18n.php
+++ TocTree.i18n.php
@@ -15,7 +15,7 @@ if ( !function_exists( 'wfJsonI18nShimbe1c9ea43bd38a62' ) ) {
 	function wfJsonI18nShimbe1c9ea43bd38a62( $cache, $code, &$cachedData ) {
 		$codeSequence = array_merge( [ $code ], $cachedData['fallbackSequence'] );
 		foreach ( $codeSequence as $csCode ) {
-			$fileName = dirname( __FILE__ ) . "/i18n/$csCode.json";
+			$fileName = __DIR__ . "/i18n/$csCode.json";
 			if ( is_readable( $fileName ) ) {
 				$data = FormatJson::decode( file_get_contents( $fileName ), true );
 				foreach ( array_keys( $data ) as $key ) {
