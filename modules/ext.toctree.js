da88b9dfb431a526bc596b46838d54811b5ae695
Fomafix <fomafix@googlemail.com>
2018-12-08 12:57:45 +0100

    Remove obsolete aliases from closures
    Bug: T208951
    Change-Id: I3e6d38e05e5d663da9dbca53e53f1d0d0731a1a0

--- modules/ext.toctree.js
+++ modules/ext.toctree.js
@@ -11,7 +11,7 @@
  * @licence GNU General Public Licence 2.0 or later
  */
 
-( function ( mw, $ ) {
+( function () {
 	function processClickEvent() {
 		var $ul = $( this ).parent().parent().children( 'ul' );
 		$ul.toggle();
@@ -71,4 +71,4 @@
 	}
 
 	mw.hook( 'wikipage.content' ).add( init );
-}( mediaWiki, jQuery ) );
+}() );
