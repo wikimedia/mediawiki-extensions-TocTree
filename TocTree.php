2d42a0a064d9350671028bc4f9e8da727d3ce466
DannyS712 <DannyS712.enwiki@gmail.com>
2020-04-05 07:58:28 +0000

    Use normal @license GPL-2.0-or-later syntax
    Change-Id: I9b442c7a60b39842e0265b5b608fa8861b297560

--- TocTree.php
+++ TocTree.php
@@ -7,7 +7,7 @@
  *
  * @author Roland Unger
  * @copyright Copyright © 2007 - 2012 Roland Unger
- * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ * @license GPL-2.0-or-later
  */
 
 if ( !defined( 'MEDIAWIKI' ) ) {
