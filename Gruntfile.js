e0400ba1d9048d47e7f9616845d52d6352ba62fa
Dringsim <dringsim@qq.com>
2024-09-22 23:55:08 +0800

    Use ES6 features
    Bug: T334853
    Change-Id: I828f70cba0c001ac1f84715387346524709084e1

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,9 +4,9 @@
  * @package TocTree
  */
 
-/* eslint-env node */
+/* eslint-env node, es6 */
 module.exports = function ( grunt ) {
-	var conf = grunt.file.readJSON( 'extension.json' );
+	const conf = grunt.file.readJSON( 'extension.json' );
 
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-eslint' );
